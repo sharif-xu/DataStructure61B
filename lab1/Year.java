35f6c57f6f9b22b80f5ab4f36e8aa0a02b63edaf
ruize xu <ruize.xu@berkeley.edu>
2020-01-23 10:03:22 -0800

    Completed Year.java

--- lab1/Year.java
+++ lab1/Year.java
@@ -1,11 +1,17 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @RUIZE.XU
  */
 public class Year {
 
     /** Return true iff YEAR is a leap year.  */
     static boolean isLeapYear(int year) {
-        return true;    // TODO: YOUR CODE HERE
+        if(year%400 == 0)
+            return true;
+        if(year%4==0 && year%100!=0)
+            return true;
+        else
+            return false;
+        // TODO: YOUR CODE HERE
     }
 
     /** Print whether YEAR is a a leap year on System.out. */
