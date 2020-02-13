e6139e4904a661933e32d7ba9abd431f724f338f
Ruize Xu <ruize.xu@berkeley.edu>
2020-02-13 01:16:12 -0800

    finish hw2 prob1

--- hw2/lists/Lists.java
+++ hw2/lists/Lists.java
@@ -5,6 +5,8 @@ package lists;
 
 /** HW #2, Problem #1. */
 
+import image.In;
+
 /** List problem.
  *  @author
  */
@@ -18,8 +20,54 @@ class Lists {
      *            ((1, 3, 7), (5), (4, 6, 9, 10), (10, 11)).
      *  Destructive: creates no new IntList items, and may modify the
      *  original list pointed to by L. */
+
     static IntListList naturalRuns(IntList L) {
-        /* *Replace this body with the solution. */
-        return null;
+        IntListList result = new IntListList();
+        IntListList result_begin = result;
+        IntList s_list = L;
+        IntList temp = L;
+        int count =  0;
+        int is_ascend = 0;
+        while (s_list != null) {
+            temp = s_list;
+            while (temp != null) {
+                if (temp.tail != null) {
+                    if (temp.head < temp.tail.head) {
+                        is_ascend = 1;  //show that the sublist is ascending
+                        count++;
+                    } else {
+                        is_ascend = 0;
+                        count++;
+                        break;
+                    }
+                } else {
+                    if (is_ascend == 1) {
+                        count++;    // the last element in the list is still ascending
+                        break;
+                    } else {
+                        count = 1;
+                        break;
+                    }
+                }
+                temp = temp.tail;
+            }
+            int[] temp_arr = new int[count]; //use to store one ascending list in the loop
+            for (int i = 0; i < count ; i++) {
+                temp_arr[i] = s_list.head;
+                s_list = s_list.tail;
+            }
+            count = 0; // for every loop reset count
+            IntList transfer = IntList.list(temp_arr); //transfer the array to IntList, which fits in the IntListList.head
+            if (s_list != null) {
+                IntListList list_shell = new IntListList();
+                result.head = transfer;
+                result.tail = list_shell;
+                result = result.tail;
+            } else {
+                result.head = transfer;
+                result.tail = null;
+            }
+        }
+        return result_begin;
     }
 }
--- hw2/lists/ListsTest.java
+++ hw2/lists/ListsTest.java
@@ -9,9 +9,17 @@ import static org.junit.Assert.*;
  */
 
 public class ListsTest {
-    /** FIXME
-     */
 
+    @Test
+    public void myListsTests() {
+        IntList test1 = IntList.list(1, 3, 7, 5, 4, 6, 9, 10, 10, 11);
+        int array[][] = {{1, 3, 7}, {5}, {4, 6, 9, 10}, {10, 11}};
+        IntList test2 = IntList.list(1, 2, 3, 1, 3, 5, 1, 6, 9, 9, 9, 10);
+        int array2[][] = {{1,2, 3}, {1, 3, 5}, {1, 6, 9}, {9}, {9, 10}};
+
+        assertEquals(IntListList.list(array), Lists.naturalRuns(test1));
+        assertEquals(IntListList.list(array2), Lists.naturalRuns(test2));
+    }
     // It might initially seem daunting to try to set up
     // IntListList expected.
     //
