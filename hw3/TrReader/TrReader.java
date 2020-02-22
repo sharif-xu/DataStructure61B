7869fb7ba2539d93b14b4982deddd7960057fcd9
Ruize Xu <ruize.xu@berkeley.edu>
2020-02-21 23:44:38 -0800

    add some comments and fix autograder error

--- hw3/TrReader/TrReader.java
+++ hw3/TrReader/TrReader.java
@@ -23,11 +23,12 @@ public class TrReader extends Reader {
     }
 
     @Override
-    public int read(char[] buf, int offset, int len) throws IOException {
-        int temp = _str.read(buf, offset, len);
-        for(int i = 0;i<buf.length;i++){
-            if(_from.indexOf(buf[i])!=-1){
-                buf[i] = _to.charAt(_from.indexOf(buf[i]));
+    public int read(char[] cbuf, int off, int len) throws IOException{
+        int temp = _str.read(cbuf, off, len);
+        for (int i = off; i < temp; i++) {
+            int index = this._from.indexOf(cbuf[i]);
+            if ( index >= 0) {
+                cbuf[i] = _to.charAt(index);
             }
         }
         return temp;
